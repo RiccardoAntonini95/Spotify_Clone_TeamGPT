2be80d3d83144e6c967e2e82d904cb792663ba46
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2023-12-14 21:56:03 +0100

    added href navs

--- assets/js/scriptArtist.js
+++ assets/js/scriptArtist.js
@@ -2,8 +2,30 @@ const artistPageDiv = document.querySelector("#artistPageHere")
 let artistaCercato = localStorage.getItem("ricerca")
 const urlDinamico = `https://deezerdevs-deezer.p.rapidapi.com/search?q={${artistaCercato}}`
 
+let param = new URLSearchParams(document.location.search)
+let artistId = param.get("id")  //if questo diverso true  --->  if(artistid){}
+let artistName = param.get("nameArtist")
+const urlParam = ''
+
+//i fetch vanno dentro un if
+if(artistId){
+//fetch del link artista coi param
+fetch(urlParam, {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a50fe14028msh940c0b8b9fece73p125787jsn3dae20f86ae0',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    }
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
 
 
+
+
+
+//fetch del localstorage
 fetch(urlDinamico, {
     method: 'GET',
     headers: {
