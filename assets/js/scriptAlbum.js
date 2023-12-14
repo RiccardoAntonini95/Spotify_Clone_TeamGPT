ee8ab837d425f21f0eb89bd855cae846ae3907e8
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2023-12-15 00:20:24 +0100

    yo

--- assets/js/scriptAlbum.js
+++ assets/js/scriptAlbum.js
@@ -4,3 +4,18 @@
 //riproduzioni ci metto rank
 
 //qua mi serviranno due cicli diversi per la parte inferiore
+
+let param = new URLSearchParams(document.location.search)
+let idAlbum = param.get("id")  
+let artistName = param.get("nameArtist")
+const urlAlbum = "https://deezerdevs-deezer.p.rapidapi.com/album/${idAlbum}"
+
+fetch(urlAlbum, {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a50fe14028msh940c0b8b9fece73p125787jsn3dae20f86ae0',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    }
+    .then(response => response.json())
+    .then(data => console.log(data))
+})
