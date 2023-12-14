a9684ebb500a16877579bcf097a5b75ed38f0b49
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2023-12-15 00:55:15 +0100

    update home

--- assets/js/scriptHome.js
+++ assets/js/scriptHome.js
@@ -21,23 +21,31 @@ fetch(urlQueen, {
             const trackQueen = data.data[i].title
             const nameQueen = data.data[i].artist.name
             const singleCard = `
-            <div class="card rounded-4 col-lg-3 my-4 box-CONSIGLI cursore-puntatore">
+            <div class="card rounded-4 col-lg-2 my-4 box-CONSIGLI cursore-puntatore">
             <div>
             <div class="d-flex flex-lg-column">
-                <div class="justify-content-center p-2">
+                <div class="justify-content-center align-self-center">
                     <a href="./album-page.html?id=${idAlbum}"><img src="${albumQueen}"
                         class="img-fluid img-CONSIGLI cursore-puntatore mt-3 " alt="copertinaAlbum"></a>
                 </div>
                 <div class="col mt-3">
-                    <p class="card-title d-lg-none m-2">Album</p>
+                    <p class="card-title d-lg-none text-riduzioneIngrandimento">Album</p>
                     <a href="#" class="text-NODECORATION text-bg-light text-decoration-none">
-                        <p class="card-text fs-4 fw-bold m-2">${trackQueen}</p>
+                        <h1 class="card-text fw-bold m-2 title-riduzioneIngrandimento">${trackQueen}</h1>
                     </a>
                     <a href="./artist-page.html?id=${idQueen}&nameArtist=${nameQueen}" class="text-NODECORATION text-bg-light text-decoration-none">
-                        <p class="card-text fs-4 d-none d-lg-block m-2">${nameQueen}</p>
+                        <p class="card-text d-none d-lg-block text-riduzioneIngrandimento mb-3">${nameQueen}</p>
                     </a>
                 </div>
              </div>
+             <div class="d-flex justify-content-between text-white  d-lg-none">
+                        <div class="d-flex ">
+                          <i class=" bi-suit-heart-fill me-3 icon-change icon-riduzioneIngrandimento"></i>
+                          <i class=" bi bi-three-dots-vertical icon-change icon-riduzioneIngrandimento"></i>
+                        </div>
+                          <p class="icon-riduzioneIngrandimento1 align-self-center">16 brani</p>
+                          <i class=" bi bi-play-circle-fill icon-change img-fluid icon-riduzioneIngrandimento "></i>
+                      </div>
            </div>
          </div>`
          contenitoreCard.innerHTML += singleCard;
